d1cfe99272a215c3dca3f3159a42a5892fc7b960
taowu750 <wutaodsg5201314@gmail.com>
2021-08-06 00:27:01 +0800

    :white_check_mark: 增加 form 数据加解密的测试

--- src/main/java/com/ncoxs/myblog/controller/test/SystemTestController.java
+++ src/main/java/com/ncoxs/myblog/controller/test/SystemTestController.java
@@ -4,10 +4,7 @@ import com.ncoxs.myblog.handler.encryption.Encryption;
 import com.ncoxs.myblog.model.dto.GenericResult;
 import com.ncoxs.myblog.model.pojo.User;
 import lombok.ToString;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -34,6 +31,25 @@ public class SystemTestController {
     @PostMapping(value = "/encryption/json")
     @Encryption
     public GenericResult<Map<String, Object>> testEncryption(@RequestBody TestEncryptionParam param) {
-        return GenericResult.success(mp(kv("message", param.message), kv("code", param.code), kv("user", param.user)));
+        return GenericResult.success(mp(kv("message", param.message), kv("code", param.code),
+                kv("user", param.user)));
+    }
+
+    /**
+     * 用来测试系统加解密是否正常的方法。参数是 form，请求方法是 post。
+     */
+    @PostMapping(value = "/encryption/form-post")
+    @Encryption
+    public GenericResult<Map<String, Object>> testEncryptionPost(String message, int code, User user) {
+        return GenericResult.success(mp(kv("message", message), kv("code", code), kv("user", user)));
+    }
+
+    /**
+     * 用来测试系统加解密是否正常的方法。参数是 form，请求方法是 get。
+     */
+    @GetMapping(value = "/encryption/form-get")
+    @Encryption
+    public GenericResult<Map<String, Object>> testEncryptionGet(String message, int code, User user) {
+        return GenericResult.success(mp(kv("message", message), kv("code", code), kv("user", user)));
     }
 }
--- src/test/java/com/ncoxs/myblog/controller/test/SystemTestControllerTest.java
+++ src/test/java/com/ncoxs/myblog/controller/test/SystemTestControllerTest.java
@@ -8,6 +8,7 @@ import com.ncoxs.myblog.model.dto.GenericResult;
 import com.ncoxs.myblog.model.pojo.User;
 import com.ncoxs.myblog.util.general.AESUtil;
 import com.ncoxs.myblog.util.general.RSAUtil;
+import com.ncoxs.myblog.util.model.FormFormatter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,7 +17,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Map;
 
@@ -28,6 +31,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * {@link SystemTestController} 测试类。
+ */
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SystemTestControllerTest {
@@ -56,26 +62,42 @@ public class SystemTestControllerTest {
         System.out.println(requestRsaPublicKey());
     }
 
-    @Test
-    public void testEncryptionJson() throws Exception {
+    private void testEncryption(String url, Map<String, Object> params, boolean paramsIsBody, boolean paramsIsJson) throws Exception {
+        Map<String, Object> rsaData = requestRsaPublicKey();
         // 获取 RSA 公钥
-        byte[] rsaPublicKey = Base64.getDecoder().decode((String) requestRsaPublicKey().get("key"));
+        byte[] rsaPublicKey = Base64.getDecoder().decode((String) rsaData.get("key"));
 
         // 生成客户端 AES 秘钥，并使用它加密数据
         byte[] aesKey = AESUtil.generateKey();
-        User user = new User();
-        user.setName("wutao");
-        user.setAge(23);
-        Map<String, Object> params = mp(kv("message", "加密信息"), kv("code", 42), kv("user", user));
-        byte[] encryptedParams = AESUtil.encrypt(aesKey, objectMapper.writeValueAsBytes(params));
+
+        // 构造 POST/GET
+        MockHttpServletRequestBuilder requestBuilder;
+        if (!paramsIsBody && !paramsIsJson) {
+            requestBuilder = get(url);
+        } else {
+            requestBuilder = post(url);
+        }
+        requestBuilder.header(HttpHeaderKey.ENCRYPTION_MODE, HttpHeaderConst.ENCRYPTION_MODE_FULL)
+                .header(HttpHeaderKey.RSA_EXPIRE, rsaData.get("expire"))
+                // 使用 RSA 公钥加密客户端 AES 秘钥
+                .header(HttpHeaderKey.REQUEST_ENCRYPTED_AES_KEY, Base64.getEncoder().encodeToString(RSAUtil.encryptByPublic(rsaPublicKey, aesKey)));
+        // 加密数据并添加到请求体/请求头中
+        byte[] encryptedParams;
+        if (paramsIsJson) {
+            requestBuilder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
+            encryptedParams = AESUtil.encrypt(aesKey, objectMapper.writeValueAsBytes(params));
+        } else {
+            requestBuilder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED);
+            encryptedParams = AESUtil.encrypt(aesKey, FormFormatter.format(params).getBytes(StandardCharsets.US_ASCII));
+        }
+        if (paramsIsBody) {
+            requestBuilder.content(encryptedParams);
+        } else {
+            requestBuilder.header(HttpHeaderKey.ENCRYPTED_PARAMS, Base64.getEncoder().encodeToString(encryptedParams));
+        }
 
         // 请求加解密测试方法
-        MvcResult mvcResult = mockMvc.perform(post("/test/system/encryption/json")
-                .header(HttpHeaderKey.ENCRYPTION_MODE, HttpHeaderConst.ENCRYPTION_MODE_FULL)
-                // 使用 RSA 公钥加密客户端 AES 秘钥
-                .header(HttpHeaderKey.REQUEST_ENCRYPTED_AES_KEY, Base64.getEncoder().encodeToString(RSAUtil.encryptByPublic(rsaPublicKey, aesKey)))
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content(encryptedParams))
+        MvcResult mvcResult = mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andExpect(header().exists(HttpHeaderKey.ENCRYPTION_MODE))
                 .andExpect(header().exists(HttpHeaderKey.REQUEST_ENCRYPTED_AES_KEY))
@@ -91,5 +113,38 @@ public class SystemTestControllerTest {
         GenericResult<Map<String, Object>> result = objectMapper.readValue(AESUtil.decrypt(aesKey, encryptedResponse),
                 new TypeReference<GenericResult<Map<String, Object>>>(){});
         System.out.println(result.getData());
+    }
+
+    /**
+     * 测试传输 JSON 加解密。
+     */
+    @Test
+    public void testEncryptionJson() throws Exception {
+        User user = new User();
+        user.setName("wutao");
+        user.setAge(23);
+        testEncryption("/test/system/encryption/json",
+                mp(kv("message", "加密信息"), kv("code", 42), kv("user", user)),
+                true, true);
+    }
+
+    /**
+     * 测试 POST 传输 form 加解密。
+     */
+    @Test
+    public void testEncryptionFormPost() throws Exception {
+        testEncryption("/test/system/encryption/form-post",
+                mp(kv("message", "加密信息"), kv("code", 42), kv("name", "wuhan"), kv("age", 23)),
+                true, false);
+    }
+
+    /**
+     * 测试 GET 传输 form 加解密。
+     */
+    @Test
+    public void testEncryptionFormGet() throws Exception {
+        testEncryption("/test/system/encryption/form-get",
+                mp(kv("message", "加密信息"), kv("code", 42), kv("name", "野兽先辈"), kv("age", 24)),
+                false, false);
     }
 }
